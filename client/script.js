beb625c3b4d784368191e90b7915ba6a88854359
RHemstad <28883539+RHemstad@users.noreply.github.com>
2023-06-29 17:20:52 -0400

    FIX: chat area scroll not working

--- client/script.js
+++ client/script.js
@@ -118,7 +118,7 @@ const handleSubmit = async (e) => {
     //get the data from the server
     //when testing set this to be http://localhost:5000
     //otherwise https://project-a-o83f.onrender.com
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://project-a-o83f.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
